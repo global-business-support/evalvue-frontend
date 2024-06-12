afe40f295f6f17516e9984bce44b41c7cb2752ce
keshav2929 <155626616+keshav2929@users.noreply.github.com>
2024-06-12 12:27:51 +0530

    screen adjust
    screen adjust

--- src/Pages/Dashboard/Organization.jsx
+++ src/Pages/Dashboard/Organization.jsx
@@ -3,8 +3,6 @@ import { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import Addorganization from "./Addorganization";
 import { UserContext } from "../../Contextfile";
-import deleteIcon from '../../assets/images/delete6.png';
-import editIcon from '../../assets/images/edit2.png';
 import Loader from "../Loader";
 import Tittle from "../../Tittle";
 import { FaClock } from "react-icons/fa6";
@@ -66,39 +64,36 @@ export default function Organization() {
       {Isorgmap ? (
         <div className="lg:px-6 sm:px-2 py-10 rounded-lg mx-auto">
           <div className="flex justify-between items-center mb-6 bg-white p-4 rounded-lg">
-            <h2 className="text-lg font-semibold">Total Organization: {Orgdata.length}</h2>
+            <h2 className="sm:text-lg text-xs font-semibold">Total Organization: {Orgdata.length}</h2>
             <NavLink to={`/dashboard/organization/addorganization`}>
-              <button className="bg-primary-100 text-white transition duration-300 border border-primary-100 text-primary-100 font-semibold py-2 px-4 rounded">
+              <button className="bg-primary-100 sm:text-base text-[12px] text-white transition duration-300 border border-primary-100 font-semibold py-2 sm:px-4 px-2 rounded">
                 + Add Organization
               </button>
             </NavLink>
           </div>
-          <div className="mb-3  flex justify-center items-start mt-8 overflow-y-auto">
+          <div className="mb-3 flex justify-center items-start mt-8 overflow-y-auto">
             <table className="w-full border-separate border-spacing-y-3">
               <thead>
                 <tr>
-                  <td className="text-left font-bold text-black  py-2 px-4">
+                  <td className="text-left font-bold text-black  py-2 sm:px-4 px-1 sm:text-base text-[12px]">
                     Organization:
                   </td>
-                  <td className="text-left font-bold text-black  py-2 px-4">Document Number:</td>
-                  <td className="text-left font-bold text-black  py-2 px-4">Address:</td>
-                  <td className="text-left font-bold text-black  py-2 px-4">View:</td>
+                  <td className="text-left font-bold text-black  py-2 sm:px-4 px-1 sm:text-base text-[12px]">Document Number:</td>
+                  <td className="text-left font-bold text-black  py-2 sm:px-4 px-1 sm:text-base text-[12px]">Address:</td>
+                  <td className="text-left font-bold text-black  py-2 sm:px-4 px-1 sm:text-base text-[12px]">View:</td>
                   {/* <td className="text-left font-bold text-black  py-2 px-4">Edit / Delete:</td> */}
                 </tr>
               </thead>
-              <tbody className=" ">
+              <tbody>
                 {Orgdata.map((organization, index) => (
                   <tr
                   key={organization.organization_id}
-                  className="lg:px-6 sm:px-2"
+                  className="lg:px-6 sm:px-5 px-1"
                   >
-
-
-                      {console.log(organization)}
-                      <td className={`py-3 px-4 w-[25%] bg-${organization.organization_verified?"white" :"[#f3f7fc]"} rounded-l-lg border-l  shadow-top-bottom-xl`}>
+                      <td className={`py-3 sm:px-4 px-1 w-[25%] bg-${organization.organization_verified ? "white" : "[#f3f7fc]"} rounded-l-lg border-l  shadow-top-bottom-xl`}>
                         <div className="flex justify-start items-center gap-2">
                         <div className="relative">
-                          <div className="h-16 w-16 rounded-full border-[2px] border-primary-100 overflow-hidden">
+                          <div className="sm:h-16 sm:w-16 w-12 rounded-full border-[2px] border-primary-100 overflow-hidden">
                             <img
                               src={organization.image}
                               alt=""
@@ -106,19 +101,22 @@ export default function Organization() {
                             />
                           </div>
                         </div>
-                        <h2 className="font-semibold text-sm text-primary-100">
+                        <h2 className="font-semibold sm:text-base text-[12px] text-primary-100">
                           {organization.name}
                         </h2>
                         </div>
                       </td>
-                      <td className={`py-3 px-4 w-[18%] text-sm bg-${organization.organization_verified?"white" :"[#f3f7fc]"} text-primary-100 font-semibold  shadow-top-bottom-xl`}>
+                      <td className={`py-3 sm:px-4 px-1 sm:text-base text-[12px] w-[18%] bg-${organization.organization_verified ? "white" : "[#f3f7fc]"} text-primary-100 font-semibold  shadow-top-bottom-xl`}>
                       {organization.document_number}
                       </td>
-                      <td className={`py-3 px-4 text-sm bg-${organization.organization_verified?"white" :"[#f3f7fc]"} text-primary-100  font-semibold  shadow-top-bottom-xl`}>
+                      <td className={`py-3 sm:px-4 px-1 sm:text-base text-[12px] bg-${organization.organization_verified ? "white" : "[#f3f7fc]"} text-primary-100  font-semibold  shadow-top-bottom-xl`}>
+                        <h1 className="sm:w-52 w-20 sm:text-wrap truncate">
                         {organization.area}, {organization.city_name},{" "}
                         {organization.state_name}, {organization.pincode}
+                        </h1>
+                        
                       </td>
-                      <td className={`ml-10 py-3 px-4 w-[10%] bg-${organization.organization_verified?"white" :"[#f3f7fc]"} text-primary-100  shadow-top-bottom-xl`}>
+                      <td className={`ml-10 py-3 sm:px-4 px-1 w-[10%] bg-${organization.organization_verified? "white" : "[#f3f7fc]"} text-primary-100  shadow-top-bottom-xl`}>
                         <NavLink
                           to={`/dashboard/organization/employee/${organization.organization_id}`}
                           state={{
@@ -129,35 +127,22 @@ export default function Organization() {
                             orgimg: organization.image,
                           }}
                         >
-                          {organization.organization_verified?
-                          
-                          <button className="  text-white flex gap-2 bg-primary-100 font-semibold py-2 px-6 rounded border border-primary-100 transition duration-300 hover:text-white text-sm">
+                          {organization.organization_verified ?
+                          <button className=" text-white flex gap-1 bg-primary-100 font-semibold py-2 lg:px-6 px-4 rounded border border-primary-100 transition duration-300 hover:text-white text-sm">
                           <BiSolidShow className="my-auto h-5 w-5"/>View 
                           </button>:
-                           <button className="text-white flex gap-2  bg-[#88898b]  font-semibold py-2 px-2 rounded border transition duration-300 hover:text-white text-sm" disabled>
+                           <button className="text-white flex gap-2  bg-[#88898b]  font-semibold py-2 sm:px-2 px-1 rounded border transition duration-300 hover:text-white sm:text-sm text-[12px]" disabled>
                            <FaClock className="my-auto h-4 w-4"/>Pending...
                            </button>  
                         }
-                          
                         </NavLink>
                       </td>
-                      {/* <td className="py-3 px-4  bg-white text-primary-100  rounded-r-lg  shadow-top-bottom-xl">
-                        <NavLink to={""}>
-                          <div className="flex gap-3 lg:ml-4">
-                            <button><img src={editIcon} alt="edit-icon" className="w-7 h-7" /></button>
-                            <button><img src={deleteIcon} alt="delete-icon" className="w-7 h-7"/></button> 
-                          </div>
-                        </NavLink>
-                      </td> */}
-                    </tr> 
-                    
-
-                    <td className="py-3 px-4  bg-white text-primary-100  rounded-r-lg  shadow-top-bottom-xl">
+                    <td className={`ml-10 py-3 sm:px-4 px-1 w-[10%] bg-${organization.organization_verified? "white" : "[#f3f7fc]"} text-primary-100 rounded-r-lg  shadow-top-bottom-xl`}>
+                    {organization.organization_verified ?
                     <ThreeDotMenu
                     onEdit={() => handleEdit(organization.organization_id)}
                     onDelete={() => handleDelete(organization.organization_id)}
-                    
-                  />
+                  /> : <div></div>}
                     </td>
                   </tr>
                 ))}
@@ -167,7 +152,7 @@ export default function Organization() {
         </div>
       ) : (
         <Addorganization userId={userId} />
-      )}
+      )};
     </>
   );
 }
