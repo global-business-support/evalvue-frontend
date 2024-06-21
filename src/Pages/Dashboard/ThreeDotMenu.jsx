c1799fa19fe8ba8447d390aee1e5542f7d72d00e
keshav2929 <155626616+keshav2929@users.noreply.github.com>
2024-06-21 16:59:33 +0530

    terminate employee button added & Organization name, image, time added when user view emp by search by aadhar
    terminate employee button added & Organization name, image, time added when user view emp by search by aadhar

--- src/Pages/Dashboard/ThreeDotMenu.jsx
+++ src/Pages/Dashboard/ThreeDotMenu.jsx
@@ -29,16 +29,19 @@ function ThreeDotMenu({ onEdit, onDelete }) {
         &#x22EE;
       </button>
       {showMenu && (
-        <div className="absolute right-0 mt-0 w-44 flex-col justify-center items-center gap-5 bg-white border border-gray-200 rounded-lg shadow-lg z-10">
+        <div className="absolute right-0 mt-0 flex-col justify-center items-center border border-gray-200 z-10">
           <button
             onClick={() => { onEdit(); setShowMenu(false); }}
-            className="block w-full text-green-500 border rounded border-green-500 px-4 py-2 text-sm  text-center hover:bg-gray-100 hover:rounded-lg"
+            className="mb-2 block w-full text-green-500 border bg-white border-green-500 sm:px-4 px-2 py-2 sm:text-sm text-[11px]  text-center hover:bg-green-500 hover:text-white"
           >
             <FiEdit className="inline w-4 h-4 mr-2" /> Edit
           </button>
-          {/* <button className="  text-red-500 border-red-500 text-center font-semibold py-2 sm:px-5 px-3 rounded border hover:shadow-sm sm:text-sm text-[11px]">
-                      Terminate
-                    </button> */}
+          <button 
+          className="  text-red-500 border-red-500 text-center bg-white font-semibold py-2 sm:px-5 px-3 border hover:shadow-sm sm:text-sm text-[11px] hover:bg-red-500 hover:text-white"
+          onClick={() => { onDelete(); setShowMenu(false); window.location.reload();}}
+          >
+            Terminate
+          </button>
 
           {/* <button
             onClick={() => { onDelete(); setShowMenu(false); }}
