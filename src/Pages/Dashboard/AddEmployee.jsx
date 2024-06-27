223abc725553eedc8ed2d4e376e5e9bcd4464ae0
Aman Chhalotre <152047708+Aman-Chhalotre@users.noreply.github.com>
2024-06-27 18:05:49 +0530

    shoot otp payload updated
    shoot otp payload updated

--- src/Pages/Dashboard/AddEmployee.jsx
+++ src/Pages/Dashboard/AddEmployee.jsx
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from "react";
 import aboutimg from "../../assets/images/about-img.jpg";
 import Loader from "../Loader";
 import Apibackendrequest from "../Apibackendrequest";
-import bgImg from '../../assets/images/FooterBackgroundImage.png'
+import bgImg from "../../assets/images/FooterBackgroundImage.png";
+import { useLocation } from "react-router-dom";
 const apiUrl = import.meta.env.VITE_API_URL;
 
 function AddEmployee() {
   const [orgList, setOrgList] = useState([]);
   const [otp, setOtp] = useState(new Array(6).fill(""));
+  const [timeLeft, setTimeLeft] = useState(120);
   const [error, setError] = useState("");
   const [errors, setErrors] = useState("");
   const [selectedOrg, setSelectedOrg] = useState({
@@ -17,22 +19,8 @@ function AddEmployee() {
   });
   const [loading, setLoading] = useState(false);
 
-  // useEffect(() => {
-  //   setLoading(true);
-  //   Apibackendrequest(`${apiUrl}/organizations/`)
-  //     .then((res) => {
-  //       setOrgList(res.data.organization_list);
-  //       if (res.isexception) {
-  //         setError(res.exceptionmessage.error);
-  //       }
-  //     })
-  //     .catch((err) => {
-  //       setError(err);
-  //     })
-  //     .finally(() => {
-  //       setLoading(false);
-  //     });
-  // }, []);
+  const location = useLocation();
+  const state = location.state;
 
   function orgHandler(e) {
     const name = e.target.name;
@@ -42,18 +30,23 @@ function AddEmployee() {
     setErrors((prevErrors) => ({ ...prevErrors, [name]: "" }));
   }
 
-  const handleEmailSubmit = async (e) => {
-    setError("");
-    setLoading(true);
-    if (e) e.preventDefault();
+  
 
+  const SendOTP = async (e) => {
+    setError("");
+    console.log("before");
+    const email = state.employee_email;
+    console.log(email);
+    e.preventDefault();
+    console.log("after");
+    console.log(state.employee_email);
     try {
       const response = await fetch(`${apiUrl}/shoot/otp/`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ email }),
+        body: JSON.stringify({ email, user_varification : false, employee_varification : true }),
       });
 
       const data = await response.json();
@@ -64,6 +57,7 @@ function AddEmployee() {
         setOtpSent(true);
         setTimeLeft(120);
         setShowResendButton(false);
+        setLoading(false);
       } else {
         setError(data.error || "Something went wrong. Please try again.");
       }
@@ -72,6 +66,66 @@ function AddEmployee() {
     }
     setLoading(false);
   };
+
+  const verifyOTP = async () => {
+    const otpCode = otp.join("");
+
+    try {
+      const response = await fetch(`${apiUrl}/verify/otp/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ user_id: user_id, otp_number: otpCode, email }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        if (
+          data.otp_verified_successfull &&
+          data.is_email_verified_successfull
+        ) {
+          setIsOtpVerified(true);
+          setIsEmailVerified(true);
+        } else if (data.otp_is_expired) {
+          setError("OTP is expired. Please request a new one.");
+        } else {
+          setError(data.error || "OTP verification failed. Please try again.");
+        }
+      } else {
+        setError(data.message || "Something went wrong. Please try again.");
+      }
+    } catch (error) {
+      setError("Failed to verify OTP. Please try again.");
+    }
+  };
+
+  const handleChange = (element, index) => {
+    if (isNaN(element.value)) return;
+
+    setOtp([...otp.map((d, idx) => (idx === index ? element.value : d))]);
+
+    if (element.nextSibling && element.value) {
+      element.nextSibling.focus();
+    }
+  };
+
+
+  // useEffect(() => {
+  //   let timer;
+  //   if (otpSent && timeLeft > 0) {
+  //     timer = setInterval(() => {
+  //       setTimeLeft((prev) => prev - 1);
+  //     }, 1000);
+  //   }
+
+  //   if (timeLeft === 0) {
+  //     setShowResendButton(true);
+  //   }
+
+  //   return () => clearInterval(timer);
+  // }, [otpSent, timeLeft]);
 
   if (loading) {
     return (
@@ -85,27 +139,31 @@ function AddEmployee() {
 
   return (
     <>
-      <div className="h-full mx-auto w-[60%] bg-white p-2 rounded-lg flex flex-col items-center">
-        <div className="h-28 w-full bg-gray-300 rounded-lg"></div>
-        <div className="w-full flex flex-col items-center gap-5 mt-[-60px]">
-          <img src={aboutimg} alt="" className="h-28 w-28 rounded-full" />
+      <div className="min-h-full mx-auto md:w-[60%] bg-white p-2 rounded-lg flex flex-col items-center">
+        <div className="h-20 w-full bg-gray-300 rounded-lg"></div>
+        <div className="w-full flex flex-col items-center mt-[-60px]">
+          <img
+            src={state?.employee_image}
+            alt=""
+            className="h-28 w-28 rounded-full"
+          />
           <div className="mt-8 text-gray-800 space-y-1">
-            <h1 className="text-xl">
-             Aman
-            </h1>
-            <h1 className="text-sm">Front End Developer
-            </h1>
-            <h1 className="text-sm">amanchhalotre@gmail.com
+            <h1 className="text-xl">{state?.employee_name}</h1>
+            <h1 className="text-sm">{state?.employee_designation}</h1>
+            <h1 className="text-sm font-bold">{state?.employee_email}</h1>
+            <h1 className="text-sm">
+              Mobile No. :{" "}
+              <span className="font-bold">{state?.employee_mobileNumber}</span>
             </h1>
             <h1 className="text-sm">
-              Mobile No. : <span className="font-bold">7845125478</span>
-            </h1>
-            <h1 className="text-sm">
-              Aadhar No. : <span className=" font-bold">458745874587</span>
+              Aadhar No. :{" "}
+              <span className=" font-bold">
+                {state?.employee_aadhar_number}
+              </span>
             </h1>
           </div>
         </div>
-        <div className="mt-5">
+        <div className="mt-5 flex flex-col">
           <label htmlFor="organization"> Please select Organization : </label>
           <select
             name="organization"
@@ -125,14 +183,21 @@ function AddEmployee() {
             </span>
           )}
         </div>
-        {/* <div className="mt-5 p-5 outline outline-primary-100 rounded h-52 w-fit text-center space-y-10">
-          <h1 className="text-lg font-medium">Please Enter Otp</h1>
-          <div>
+
+        <>
+          <div className="flex justify-between items-center mt-10">
+            <h2 className="font-bold text-xl">Please Enter OTP</h2>
+          </div>
+          <p className="text-sm text-zinc-600 mt-4">
+            Please enter the 6-digit OTP that was sent to your email
+            <span className="text-red-500"> *</span>
+          </p>
+          <div className="w-[60%] flex justify-between gap-2 mt-4">
             {otp.map((data, index) => (
               <input
                 key={index}
                 type="text"
-                className="w-10 bg-zinc-200 rounded mr-2 p-2 text-center"
+                className="w-full bg-zinc-200 rounded p-2 text-center"
                 maxLength={1}
                 value={data}
                 onChange={(e) => handleChange(e.target, index)}
@@ -141,14 +206,44 @@ function AddEmployee() {
               />
             ))}
           </div>
-        </div> */}
-        <div className="">
-          <button className="mt-5 px-8 py-2 bg-primary-100 rounded-lg text-white">
-            Sent otp
-            Confirm
-            Submit
+          {error && <p className="text-red-500 text-center mt-4">{error}</p>}
+
+          {timeLeft > 0 && (
+            <p className="text-sm text-center mt-4">
+              OTP is valid for {timeLeft} seconds
+            </p>
+          )}
+          {
+            <p className="text-sm text-center mt-4">
+              Didn't receive an email?
+              <button
+                className="text-primary-100 hover:text-blue-600 font-semibold"
+                onClick={() => {
+                  handleEmailSubmit();
+                }}
+              >
+                RESEND OTP
+              </button>
+            </p>
+          }
+        </>
+        <>
+          <button
+            onClick={(e) => {
+              SendOTP(e);
+            }}
+            className=" mt-5 px-8 py-2 bg-primary-100 rounded-lg text-white"
+          >
+            Send otp
           </button>
-        </div>
+          {/* <button
+          onClick={()=>{
+            verifyOTP()
+          }}
+          className=" mt-5 px-8 py-2 bg-primary-100 rounded-lg text-white">
+            Confirm
+          </button> */}
+        </>
       </div>
     </>
   );
--- src/Pages/Passwordotp.jsx
+++ src/Pages/Passwordotp.jsx
@@ -1,12 +1,12 @@
-import { useContext, useEffect, useState } from 'react';
-import { UserContext } from '../Contextfile';
-import { useLocation, useNavigate } from 'react-router-dom';
-import Tittle from '../Tittle';
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "../Contextfile";
+import { useLocation, useNavigate } from "react-router-dom";
+import Tittle from "../Tittle";
 import Loader from "./Loader";
 const apiUrl = import.meta.env.VITE_API_URL;
 function Passwordotp(props) {
-  Tittle("Verified OTP - Evalvue")
-  const [otp, setOtp] = useState(new Array(6).fill(''));
+  Tittle("Verified OTP - Evalvue");
+  const [otp, setOtp] = useState(new Array(6).fill(""));
   const [otpSent, setOtpSent] = useState(false);
   const [timeLeft, setTimeLeft] = useState(120);
   const [showResendButton, setShowResendButton] = useState(false);
@@ -20,12 +20,21 @@ function Passwordotp(props) {
   const location = useLocation();
   const state = location.state;
   const [loading, setLoading] = useState(false);
-
+  const [userverification, setuserverification] = useState(false);
+  const [employeeverification, setemployeeverification] = useState(false);
+  console.log(state);
   // Initialize user_id from context
   let user_id = userId;
 
   useEffect(() => {
     // Set the email state if it exists in location state and not in forget mode
+    if(state.isForget){
+      setuserverification(true)
+      setemployeeverification(false)
+    }else if(state.addemp){
+      setuserverification(false)
+      setemployeeverification(true)
+    }
     if (!state.isForget) {
       setEmail(state.email);
     }
@@ -37,21 +46,21 @@ function Passwordotp(props) {
 
     // Navigate to password generation page if OTP is verified and it is for forget password
     if (isOtpVerified && state.isForget) {
-      navigate('/passgenerate');
+      navigate("/passgenerate");
     }
   }, [isOtpVerified, state.isForget, navigate, email]);
 
   // Function to handle email submission
   const handleEmailSubmit = async (e) => {
-    setError("")
+    setError("");
     setLoading(true);
     if (e) e.preventDefault();
 
     try {
       const response = await fetch(`${apiUrl}/shoot/otp/`, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ email }),
       });
@@ -65,10 +74,10 @@ function Passwordotp(props) {
         setTimeLeft(120);
         setShowResendButton(false);
       } else {
-        setError(data.error || 'Something went wrong. Please try again.');
+        setError(data.error || "Something went wrong. Please try again.");
       }
     } catch (error) {
-      setError('Failed to send email. Please try again.');
+      setError("Failed to send email. Please try again.");
     }
     setLoading(false);
   };
@@ -86,44 +95,53 @@ function Passwordotp(props) {
 
   // Function to handle OTP input key down (Backspace)
   const handleKeyDown = (element, index, event) => {
-    if (event.key === 'Backspace') {
+    if (event.key === "Backspace") {
       if (element.previousSibling && !element.value) {
         element.previousSibling.focus();
       }
-      setOtp([...otp.map((d, idx) => (idx === index ? '' : d))]);
+      setOtp([...otp.map((d, idx) => (idx === index ? "" : d))]);
     }
   };
 
   // Function to handle OTP submission
   const handleOtpSubmit = async () => {
-    const otpCode = otp.join('');
+    const otpCode = otp.join("");
 
     try {
       const response = await fetch(`${apiUrl}/verify/otp/`, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify({ user_id: user_id, otp_number: otpCode, email }),
+        body: JSON.stringify({
+          user_id: user_id,
+          otp_number: otpCode,
+          email,
+          user_verification: userverification,
+          employee_verification: employeeverification,
+        }),
       });
 
       const data = await response.json();
 
       if (response.ok) {
-        if (data.otp_verified_successfull && data.is_email_verified_successfull) {
+        if (
+          data.otp_verified_successfull &&
+          data.is_email_verified_successfull
+        ) {
           setIsOtpVerified(true);
           setIsEmailVerified(true);
         } else if (data.otp_is_expired) {
-          setError('OTP is expired. Please request a new one.');
+          setError("OTP is expired. Please request a new one.");
         } else {
-          setError(data.error || 'OTP verification failed. Please try again.');
+          setError(data.error || "OTP verification failed. Please try again.");
         }
       } else {
-        setError(data.message || 'Something went wrong. Please try again.');
+        setError(data.message || "Something went wrong. Please try again.");
       }
     } catch (error) {
-      setError('Failed to verify OTP. Please try again.');
-    };
+      setError("Failed to verify OTP. Please try again.");
+    }
   };
 
   // Function to handle resend OTP
@@ -131,7 +149,7 @@ function Passwordotp(props) {
     let timer;
     if (otpSent && timeLeft > 0) {
       timer = setInterval(() => {
-        setTimeLeft(prev => prev - 1);
+        setTimeLeft((prev) => prev - 1);
       }, 1000);
     }
 
@@ -142,12 +160,14 @@ function Passwordotp(props) {
     return () => clearInterval(timer);
   }, [otpSent, timeLeft]);
 
-  if(loading){
-    return <>
-      <div className=' w-full h-[100vh] flex justify-center items-center'>
-        <Loader />
-      </div>
-    </>
+  if (loading) {
+    return (
+      <>
+        <div className=" w-full h-[100vh] flex justify-center items-center">
+          <Loader />
+        </div>
+      </>
+    );
   }
 
   return (
@@ -159,11 +179,17 @@ function Passwordotp(props) {
             !isEmailSent ? (
               state.isForget ? (
                 <>
-                  <h2 className="text-center text-2xl font-semibold mb-6">Send Verification Email</h2>
-                  {error && <p className="text-red-500 text-center mb-4">{error}</p>}
+                  <h2 className="text-center text-2xl font-semibold mb-6">
+                    Send Verification Email
+                  </h2>
+                  {error && (
+                    <p className="text-red-500 text-center mb-4">{error}</p>
+                  )}
                   <form onSubmit={handleEmailSubmit}>
                     <div className="mb-4">
-                      <label htmlFor="email" className="block mb-2">Email</label>
+                      <label htmlFor="email" className="block mb-2">
+                        Email
+                      </label>
                       <input
                         type="email"
                         id="email"
@@ -184,11 +210,16 @@ function Passwordotp(props) {
               ) : (
                 <>
                   <div className="flex justify-between items-center">
-                    <h2 className="font-bold text-xl">OTP Verification <span className='text-[red]'>*</span></h2>
-                    <button className="text-zinc-400 hover:text-zinc-600"
-                      onClick={() => window.location.href = "/login"}
+                    <h2 className="font-bold text-xl">
+                      OTP Verification <span className="text-[red]">*</span>
+                    </h2>
+                    <button
+                      className="text-zinc-400 hover:text-zinc-600"
+                      onClick={() => (window.location.href = "/login")}
                     >
-                      <span aria-hidden="true" className='text-3xl'>×</span>
+                      <span aria-hidden="true" className="text-3xl">
+                        ×
+                      </span>
                     </button>
                   </div>
                   <p className="text-sm text-zinc-600 mt-4">
@@ -203,46 +234,54 @@ function Passwordotp(props) {
                         className="w-full bg-zinc-200 rounded p-2 text-center"
                         maxLength={1}
                         value={data}
-                        onChange={e => handleChange(e.target, index)}
-                        onKeyDown={e => handleKeyDown(e.target, index, e)}
-                        onFocus={e => e.target.select()}
+                        onChange={(e) => handleChange(e.target, index)}
+                        onKeyDown={(e) => handleKeyDown(e.target, index, e)}
+                        onFocus={(e) => e.target.select()}
                       />
                     ))}
                   </div>
-                  {error && <p className="text-red-500 text-center mt-4">{error}</p>}
+                  {error && (
+                    <p className="text-red-500 text-center mt-4">{error}</p>
+                  )}
                   <button
                     className="mt-6 bg-primary-100 hover:bg-indigo-500 text-white font-bold py-2 px-4 rounded w-full"
                     onClick={handleOtpSubmit}
                   >
                     Continue
                   </button>
-                  {
-                  otpSent && timeLeft > 0 &&
-                  <p className="text-sm text-center mt-4">
-                    OTP is valid for {timeLeft} seconds
-                  </p>
-                }
-                {
-                  showResendButton &&
-                  <p className="text-sm text-center mt-4">
-                    Didn't receive an email?
-                    <button className="text-primary-100 hover:text-blue-600 font-semibold" 
-                    onClick={()=>{handleEmailSubmit()}}
-                    >
-                      RESEND OTP
-                    </button>
-                  </p>
-                }
+                  {otpSent && timeLeft > 0 && (
+                    <p className="text-sm text-center mt-4">
+                      OTP is valid for {timeLeft} seconds
+                    </p>
+                  )}
+                  {showResendButton && (
+                    <p className="text-sm text-center mt-4">
+                      Didn't receive an email?
+                      <button
+                        className="text-primary-100 hover:text-blue-600 font-semibold"
+                        onClick={() => {
+                          handleEmailSubmit();
+                        }}
+                      >
+                        RESEND OTP
+                      </button>
+                    </p>
+                  )}
                 </>
               )
             ) : (
               <>
                 <div className="flex justify-between items-center">
-                  <h2 className="font-bold text-xl">OTP Verification <span className='text-[red]'>*</span></h2>
-                  <button className="text-zinc-400 hover:text-zinc-600"
-                    onClick={() => window.location.href = "/login"}
+                  <h2 className="font-bold text-xl">
+                    OTP Verification <span className="text-[red]">*</span>
+                  </h2>
+                  <button
+                    className="text-zinc-400 hover:text-zinc-600"
+                    onClick={() => (window.location.href = "/login")}
                   >
-                    <span aria-hidden="true" className='text-3xl'>×</span>
+                    <span aria-hidden="true" className="text-3xl">
+                      ×
+                    </span>
                   </button>
                 </div>
                 <p className="text-sm text-zinc-600 mt-4">
@@ -257,47 +296,56 @@ function Passwordotp(props) {
                       className="w-full bg-zinc-200 rounded p-2 text-center"
                       maxLength={1}
                       value={data}
-                      onChange={e => handleChange(e.target, index)}
-                      onKeyDown={e => handleKeyDown(e.target, index, e)}
-                      onFocus={e => e.target.select()}
+                      onChange={(e) => handleChange(e.target, index)}
+                      onKeyDown={(e) => handleKeyDown(e.target, index, e)}
+                      onFocus={(e) => e.target.select()}
                     />
                   ))}
                 </div>
-                {error && <p className="text-red-500 text-center mt-4">{error}</p>}
+                {error && (
+                  <p className="text-red-500 text-center mt-4">{error}</p>
+                )}
                 <button
                   className="mt-6 bg-primary-100 hover:bg-indigo-500 text-white font-bold py-2 px-4 rounded w-full"
                   onClick={handleOtpSubmit}
                 >
                   Continue
                 </button>
-                {
-                  otpSent && timeLeft > 0 &&
+                {otpSent && timeLeft > 0 && (
                   <p className="text-sm text-center mt-4">
                     OTP is valid for {timeLeft} seconds
                   </p>
-                }
-                {
-                  showResendButton &&
+                )}
+                {showResendButton && (
                   <p className="text-sm text-center mt-4">
                     Didn't receive an email?
-                    <button className="text-primary-100 hover:text-blue-600 font-semibold" 
-                    onClick={()=>{handleEmailSubmit(); console.log("clicked")}}
+                    <button
+                      className="text-primary-100 hover:text-blue-600 font-semibold"
+                      onClick={() => {
+                        handleEmailSubmit();
+                        console.log("clicked");
+                      }}
                     >
                       RESEND OTP
                     </button>
                   </p>
-                }
+                )}
               </>
             )
           ) : (
             <div className="text-center">
-              <h2 className="text-2xl font-semibold mb-4">OTP Verification Successful</h2>
+              <h2 className="text-2xl font-semibold mb-4">
+                OTP Verification Successful
+              </h2>
               {isEmailVerified && (
                 <>
-                  <p className="text-zinc-600 mb-4">Your email has been successfully verified. You can now log in.</p>
+                  <p className="text-zinc-600 mb-4">
+                    Your email has been successfully verified. You can now log
+                    in.
+                  </p>
                   <button
                     className="bg-primary-100 text-white p-2 rounded-lg hover:bg-primary-100 focus:outline-none focus:ring-2 focus:ring-primary-100"
-                    onClick={() => navigate('/login')}
+                    onClick={() => navigate("/login")}
                   >
                     Go to Login
                   </button>
