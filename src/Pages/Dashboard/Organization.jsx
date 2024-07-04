9f3040452a25ceff6fc9326ab68434fcd7985489
Aman Chhalotre <152047708+Aman-Chhalotre@users.noreply.github.com>
2024-07-04 18:03:40 +0530

    reject message field added in verify  organization
    reject message field added in verify  organization

--- src/Pages/Dashboard/Organization.jsx
+++ src/Pages/Dashboard/Organization.jsx
@@ -6,10 +6,11 @@ import Loader from "../Loader";
 import Tittle from "../../Tittle";
 import { FaClock } from "react-icons/fa6";
 import { BiSolidShow } from "react-icons/bi";
-import { BsCreditCard2Back } from "react-icons/bs"; 
 import { FaIndianRupeeSign } from "react-icons/fa6";
-import { AiOutlineCloudUpload } from "react-icons/ai";
+import { IoReceiptOutline } from "react-icons/io5";
+import { RxCross1 } from "react-icons/rx";
 
+import { AiOutlineCloudUpload } from "react-icons/ai";
 
 import { BsPatchExclamationFill } from "react-icons/bs";
 
@@ -21,10 +22,11 @@ const apiUrl = import.meta.env.VITE_API_URL;
 export default function Organization() {
   Tittle("Organization - Evalvue");
   const [Orgdata, setOrgdata] = useState([]);
-  const [count, setCount] = useState()
+  const [count, setCount] = useState();
   const [loading, setLoading] = useState(true); // Set initial loading state to true
   const [Isorgmap, setIsorgmap] = useState(false);
   const { userId } = useContext(UserContext);
+  const [paymentSuccessfull, setPaymentSuccessfull] = useState(false);
   const [address, setAddress] = useState({});
   const [error, setError] = useState();
 
@@ -37,7 +39,6 @@ export default function Organization() {
       .then((res) => {
         setOrgdata(res.data.organization_list);
         setCount(res.data.organizations_paid_count);
-       
 
         if (res.data.is_organization_mapped) {
           setIsorgmap(res.data.is_organization_mapped);
@@ -89,9 +90,13 @@ export default function Organization() {
     navigate("/dashboard/organization/addorganization", {});
   };
 
-  function CreatePayment(organizationId){
-   const response= axios.post(`${apiUrl}/create/subscription/id/`,{user_id:userId,organization_id:organizationId,plan_id:1})
-   console.log(response)
+  function CreatePayment(organizationId) {
+    const response = axios.post(`${apiUrl}/create/subscription/id/`, {
+      user_id: userId,
+      organization_id: organizationId,
+      plan_id: 1,
+    });
+    console.log(response);
   }
 
   if (loading) {
@@ -115,7 +120,55 @@ export default function Organization() {
   if (Isorgmap) {
     return (
       <>
-        <div className="lg:px-4 sm:px-2 relative rounded-lg mx-auto ">
+        <div className="lg:px-4 sm:px-2 relative rounded-lg mx-auto">
+          {paymentSuccessfull && (
+            <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+              <div className=" lg:min-w-[500px] md:min-w-[400px] min-w-[350px]  min-h-[500px] bg-white p-5 rounded-lg shadow-lg max-w-md border-t-4 border-primary-100">
+                <div className="w-full flex justify-center mb-2">
+                  <div className="w-auto p-3 rounded-full bg-gray-200 flex items-center justify-center">
+                    <IoReceiptOutline className="text-3xl text-primary-100" />
+                  </div>
+                </div>
+                <hr className="" />
+                <div className="h-full w-full mt-2">
+                  <h1 className="text-lg">Hello, organization name</h1>
+                  <div className="bg-gray-300 w-full h-full my-5 text-sm py-1 px-3 flex flex-col items-center justify-center rounded-lg ">
+                    <p className="w-full flex justify-between">
+                      Order Id :{" "}
+                      <span className="font-semibold">455655555555555</span>
+                    </p>
+                    <p className="w-full flex justify-between">
+                      Payment Id :{" "}
+                      <span className="font-semibold">455655555555555</span>
+                    </p>
+                    <p className="w-full flex justify-between">
+                      Subscription Id :{" "}
+                      <span className="font-semibold">455655555555555</span>
+                    </p>
+                  <div className="w-full flex items-center justify-between mt-5">
+                    <h1 className="text-primary-100 text-base">Amount</h1>
+                    <h1 className="flex items-center text-xl font-semibold text-primary-100">
+                      <FaIndianRupeeSign className="text-base" />
+                      99
+                    </h1>
+                  </div>
+                  </div>
+                  <hr />
+                  <div className="w-full flex flex-col items-center text-center gap-5 mt-5">
+                    <h1 className="text-primary-100 font-semibold">
+                      Amount Paid Successfully
+                    </h1>
+                    <button
+                      className="bg-primary-100 w-40 rounded-lg py-2 px-5 text-white font-semibold"
+                      onClick={() => setPaymentSuccessfull(false)}
+                    >
+                      Ok
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
           <div className="flex justify-between sticky lg:top-[55px] top-[48px] z-[2] items-center lg:mb-3 mb-12 bg-white p-4 rounded shadow-lg">
             <h2 className="sm:text-lg text-xs font-semibold">
               Total Organization: {Orgdata.length}
@@ -155,7 +208,8 @@ export default function Organization() {
                   >
                     <td
                       className={`py-3 sm:px-2 px-1 max-w-[180px] bg-${
-                        organization.organization_verified && organization.organization_paid
+                        organization.organization_verified &&
+                        organization.organization_paid
                           ? "white"
                           : "[#f3f7fc]"
                       } rounded-l-lg border-l  shadow-top-bottom-xl`}
@@ -177,7 +231,8 @@ export default function Organization() {
                     </td>
                     <td
                       className={`py-3 sm:px-4 px-1  md:table-cell hidden sm:text-base text-[12px] max-w-[100px]  bg-${
-                        organization.organization_verified  && organization.organization_paid
+                        organization.organization_verified &&
+                        organization.organization_paid
                           ? "white"
                           : "[#f3f7fc]"
                       } text-primary-100 font-semibold  shadow-top-bottom-xl`}
@@ -188,7 +243,8 @@ export default function Organization() {
                     </td>
                     <td
                       className={`py-3 sm:px-4 px-1 md:table-cell hidden  sm:text-base text-[12px] bg-${
-                        organization.organization_verified  && organization.organization_paid
+                        organization.organization_verified &&
+                        organization.organization_paid
                           ? "white"
                           : "[#f3f7fc]"
                       } text-primary-100  font-semibold  shadow-top-bottom-xl`}
@@ -201,59 +257,62 @@ export default function Organization() {
 
                     <td
                       className={` py-3 sm:px-0 px-1 ${
-                        organization.organization_verified  && organization.organization_paid
+                        organization.organization_verified &&
+                        organization.organization_paid
                           ? "bg-white"
                           : "bg-[#f3f7fc]"
                       } text-primary-100 rounded-r-lg  shadow-top-bottom-xl`}
                     >
                       <div className="flex gap-4 justify-end items-center">
-                      { (organization.organization_rejected) ? 
-                          (<button
+                        {organization.organization_rejected ? (
+                          <button
                             className="text-white flex gap-2 mr-[45px]  font-semibold py-2 sm:px-2 px-1 rounded order-red-500 transition duration-300 bg-red-800 cursor-pointer hover:text-white sm:text-sm text-[12px]"
                             disabled
                           >
                             <AiOutlineCloudUpload className="my-auto font-semibold h-5 w-5" />
                             {/* Rejected  */}
                             Re-Apply
-                          </button>)
-                            : 
-                            (organization.organization_paid) ? 
-                                (organization.organization_verified) ?
-                                <NavLink
-                                  to={`/dashboard/organization/employee/${organization.organization_id}`}
-                                  state={{
-                                    organization_name: organization.name,
-                                    orgarea: organization.area,
-                                    orgcity: organization.city_name,
-                                    orgstate: organization.state_name,
-                                    orgimg: organization.image,
-                                  }}
-                                >
-                                  <button className=" text-white flex gap-1 bg-primary-100 font-semibold py-2 sm:px-6 px-4 rounded border border-primary-100 hover:bg-[#5559af] hover:shadow-sm hover:text-white text-sm">
-                                    <BiSolidShow className=" h-5 w-5" />
-                                    View
-                                  </button>{" "}
-                                </NavLink>
-                                :
-                                <button
-                                className="text-white flex gap-2 mr-10 bg-[#88898b]  font-semibold py-2 sm:px-2 px-1 rounded border transition duration-300 hover:text-white sm:text-sm text-[12px]"
-                                disabled
-                                >
-                                  <FaClock className="my-auto h-4 w-4" />
-                                  Pending...
-                              </button>
-                            :
+                          </button>
+                        ) : organization.organization_paid ? (
+                          organization.organization_verified ? (
+                            <NavLink
+                              to={`/dashboard/organization/employee/${organization.organization_id}`}
+                              state={{
+                                organization_name: organization.name,
+                                orgarea: organization.area,
+                                orgcity: organization.city_name,
+                                orgstate: organization.state_name,
+                                orgimg: organization.image,
+                              }}
+                            >
+                              <button className=" text-white flex gap-1 bg-primary-100 font-semibold py-2 sm:px-6 px-4 rounded border border-primary-100 hover:bg-[#5559af] hover:shadow-sm hover:text-white text-sm">
+                                <BiSolidShow className=" h-5 w-5" />
+                                View
+                              </button>{" "}
+                            </NavLink>
+                          ) : (
                             <button
+                              className="text-white flex gap-2 mr-10 bg-[#88898b]  font-semibold py-2 sm:px-2 px-1 rounded border transition duration-300 hover:text-white sm:text-sm text-[12px]"
+                              disabled
+                            >
+                              <FaClock className="my-auto h-4 w-4" />
+                              Pending...
+                            </button>
+                          )
+                        ) : (
+                          <button
                             className="flex items-center gap-2 mr-10 font-semibold py-1 sm:px-4 px-3 rounded border-2 border-primary-100 transition duration-300 bg-primary-100 text-white sm:text-base text-[14px]"
-                            onClick={()=>{
-                              
-                              CreatePayment(organization.organization_id)}}
+                            onClick={() => {
+                              CreatePayment(organization.organization_id);
+                            }}
                           >
                             Pay
-                            
-                          <span className="flex sm:text-lg text-base"><FaIndianRupeeSign className="my-auto h-4 w-4" />{count == 0 ? '1' : '99'}</span>
+                            <span className="flex sm:text-lg text-base">
+                              <FaIndianRupeeSign className="my-auto h-4 w-4" />
+                              {count == 0 ? "1" : "99"}
+                            </span>
                           </button>
-                      }
+                        )}
 
                         {/* {organization.organization_verified ? (
                           <NavLink
